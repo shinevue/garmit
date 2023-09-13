cffaf9539c703df5fa6de50926d075fb5ac21217
shine <shine401020@gmail.com>
2023-09-13 08:07:59 +0800

    fixed: try to install three

--- WebApplication/Scripts/components/Assets/FloorMap/MapLayer/LayoutObject.jsx
+++ WebApplication/Scripts/components/Assets/FloorMap/MapLayer/LayoutObject.jsx
@@ -11,6 +11,7 @@ import PropTypes from 'prop-types';
 
 import RectObjectGroup from 'Assets/FloorMap/Object/RectObjectGroup/RectObjectGroup.jsx';
 import { OBJECT_TYPE } from 'constant';
+import * as THREE from 'three';
 
 /**
  * レイアウトオブジェクトレイヤ
@@ -24,7 +25,36 @@ export default class LayoutObject extends Component {
         this.state = {
         };
     }
+    init() {
+        const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+        camera.position.z = 1;
+        
+        const scene = new THREE.Scene();
+        
+        const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+        const material = new THREE.MeshNormalMaterial();
+        
+        const mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+        
+        const renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setAnimationLoop( animation );
+        document.body.appendChild( renderer.domElement );
+    }
 
+    componentDidMount() {
+        this.init();
+        function animation( time ) {
+        
+            mesh.rotation.x = time / 2000;
+            mesh.rotation.y = time / 1000;
+        
+            renderer.render( scene, camera );
+        
+        }
+        animation(4);
+    }
     /**********************************
     * 
     * ライフサイクルイベント
